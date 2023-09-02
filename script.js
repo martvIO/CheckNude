f4323b84a7490ba118beb6f9db2eac57031b60ef
martvIO <136852336+martvIO@users.noreply.github.com>
2023-09-03 01:09:21 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+fetch('/api/data')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data.message);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
